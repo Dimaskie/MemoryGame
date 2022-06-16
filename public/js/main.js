a5d0e722daf678babd22b38183f6ff1b9004de49
Dimaskie <brun0yuj1ym02@gmail.com>
2022-06-16 10:01:03 +0900

    alerts

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,31 @@
 const card = document.querySelectorAll('.card')
-console.log(card)
-for(i = 0; i<card.length; i++){
-    card[i].onclick = function(){
-        console.log(this)
-        this.style.visibility ='hidden'
+
+function flip(){
+    let count = 0;
+    let clicked = []
+    for(i = 0; i<card.length; i++){
+        card[i].onclick = function(){
+            if(count<2){
+                this.style.visibility ='hidden'
+                console.log(clicked)
+                if(count<2){
+                clicked.push(this)
+                }
+                console.log(clicked)
+            }
+            
+            count++
+            
+            if(count == 2){
+
+                if(clicked[0].className == clicked[1].className){
+                    alert("Well Done !")
+                }
+                else{
+                    alert("Wrong!")
+                }
+            }
+        }
     }
 }
+flip();
